87bf2e103d77c78871e19841cd015c24ef42538e
GunZir <icekrung.thanakon@gmail.com>
2024-02-19 12:15:46 +0700

    update apps directory

--- ros2_ws/apps/polaris_controller_interface/app.c
+++ ros2_ws/apps/polaris_controller_interface/app.c
@@ -8,6 +8,8 @@
 #include <messages/msg/motor_robot_speed.h>
 #include <geometry_msgs/msg/twist.h>
 
+#include <sensor_msgs/msg/joint_state.h>
+
 #include "libs/e12Com.h"
 
 #include <rclc/rclc.h>
@@ -21,28 +23,23 @@
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
 
+#define WHEEL_BASE 0.125
+// #define WHEEL_RADIUS 0.0625
+#define GAMMA  2994.6594
 
 rcl_publisher_t publisher;
 rcl_subscription_t subscriber;
 
-messages__msg__MotorRobotSpeed encoder_msg;
 geometry_msgs__msg__Twist cmd_vel;
-
-rcl_allocator_t allocator = rcl_get_default_allocator();
-rclc_support_t support;
-
-rcl_timer_t timer;
-const unsigned int timer_timeout = 10;
-
-rclc_executor_t executor;
+messages__msg__MotorRobotSpeed encoder_msg;
+// sensor_msgs__msg__JointState joint_stat;
 
 typedef struct {
     double left;
     double right;
-} MotorSpeeds;
+} MotorPPS;
 
-const int64_t WHEEL_BASE;
-const int64_t WHEEL_RADIUS;
+
 
 void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
 {
@@ -60,23 +57,19 @@ void subscription_callback(const void * msgin)
 {
 	const geometry_msgs__msg__Twist * msg = (const geometry_msgs__msg__Twist *)msgin;
 
-	MotorSpeeds speeds;
+	MotorPPS PPS;
 
-	speeds.left = (msg->linear.x - msg->angular.z * WHEEL_BASE / 2.0);
-    speeds.right = (msg->linear.x + msg->angular.z * WHEEL_BASE / 2.0);
+	PPS.left = -(msg->linear.x - msg->angular.z * WHEEL_BASE / 2.0) * GAMMA;  // pulse per sec
+    PPS.right = (msg->linear.x + msg->angular.z * WHEEL_BASE / 2.0) * GAMMA;
 
-	speeds.left /= (WHEEL_RADIUS * 2 * M_PI) / 60.0;
-    speeds.right /= (WHEEL_RADIUS * 2 * M_PI) / 60.0;
-
-	dataESP2STM.speedB = speeds.right;
-	dataESP2STM.speedA = speeds.left;
+	dataESP2STM.speedB = PPS.right;
+	dataESP2STM.speedA = PPS.left;
 	
 }
 
-void e12_init()
+void appMain(void * arg)
 {
 	E12_init();
-
 	dataESP2STM.kp = 2.05;
 	dataESP2STM.ki = 9;
 	dataESP2STM.kd = 0.0005;
@@ -84,17 +77,21 @@ void e12_init()
 
 	encoder_msg.right_motor_speed = dataSTM2ESP.encoderB;
 	encoder_msg.left_motor_speed = dataSTM2ESP.encoderA;
-}
 
-void micro_ros_init()
-{
+	rcl_allocator_t allocator = rcl_get_default_allocator();
+	rclc_support_t support;
+	
+
+	rcl_node_t node;
+
 	// create init_options
 	RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
 
 	// create node
-	rcl_node_t node;
 	RCCHECK(rclc_node_init_default(&node, "polaris_controller_interface", "", &support));
 
+	rcl_timer_t timer;
+	const unsigned int timer_timeout = 10;
 	// create publisher
 	RCCHECK(rclc_publisher_init_best_effort(
 		&publisher,
@@ -107,7 +104,7 @@ void micro_ros_init()
 		&subscriber,
 		&node,
 		ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
-		"/polaris/cmd_vel"));
+		"/diff_cont/cmd_vel_unstamped"));
 
 	
 	// create timer,
@@ -117,6 +114,7 @@ void micro_ros_init()
 		RCL_MS_TO_NS(timer_timeout),
 		timer_callback));
 
+	rclc_executor_t executor;
 	// create executor
 	RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
 
@@ -125,12 +123,6 @@ void micro_ros_init()
 
     // add subscriber callback
     RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &cmd_vel, &subscription_callback, ON_NEW_DATA));
-}
-
-void appMain(void * arg)
-{
-	e12_init();
-	micro_ros_init();
 	
 	while(1){
 		rclc_executor_spin_some(&executor, RCL_MS_TO_NS(1));
